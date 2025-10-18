9912b0ec8cf7a926d9dd3ba9b00cfc09192865b0
Nirmal-Kharal <nirmalkharal40@gmail.com>
2025-10-18 21:21:42 +0545

    feat(tailwind):setup tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const page = () => {
   return (
     <div>
-      <h1 className="text-violet-800 text-4xl text-center">
+      <h1 className="bg-red-500 text-light-500 text-4xl">
+        Hello ,welcome to God-Nyxen World🤭
+      </h1>
+      <h1 className="bg-red-500 font-bold text-light-500 text-4xl">
         Hello ,welcome to God-Nyxen World🤭
       </h1>
     </div>
